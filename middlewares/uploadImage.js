8c701dd72db6596117bcf3e81fe3141225c306c6
Ngoc Sam <sykormusic@gmail.com>
2023-12-08 20:11:39 +0700

    Fix product & extend image size

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -23,7 +23,7 @@ const multerFilter = (req, file, cb) => {
 const uploadPhoto = multer({
   storage: storage,
   fileFilter: multerFilter,
-  limits: { fileSize: 1000000 },
+  limits: { fileSize: 2000000 },
 });
 
 const productImgResize = async (req, res, next) => {
