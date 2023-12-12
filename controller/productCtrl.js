cb875b0982e691e36e4df7d12fbc9d1fb764d4dd
Ngoc Sam <sykormusic@gmail.com>
2023-12-12 22:27:07 +0700

    fix rating populate

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -47,7 +47,7 @@ const getaProduct = asyncHandler(async (req, res) => {
   const { id } = req.params;
   validateMongoDbId(id);
   try {
-    const findProduct = await Product.findById(id);
+    const findProduct = await Product.findById(id).populate("ratings.postedby");
     res.json(findProduct);
   } catch (error) {
     throw new Error(error);
